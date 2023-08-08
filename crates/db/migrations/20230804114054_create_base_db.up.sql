633060b066d840058625eb08e2b8de17f5bfcad2
Arthurdw <dev@arthurdw.com>
2023-08-08 11:51:01 +0200

    :recycle: Removed the db submodule

---
+++ crates/db/migrations/20230804114054_create_base_db.up.sql
@@ -0,0 +1,63 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS accounts (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  contact_email VARCHAR(255) NOT NULL UNIQUE,
+  encryption_method SMALLSERIAL NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS accounts_email_idx ON accounts (email);
+CREATE INDEX IF NOT EXISTS accounts_contact_email_idx ON accounts (contact_email);
+
+CREATE TABLE IF NOT EXISTS links (
+  type SMALLSERIAL NOT NULL,
+  id VARCHAR(255) NOT NULL,
+  encryption_method SMALLSERIAL NOT NULL,
+  account_id UUID NOT NULL REFERENCES accounts(id),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  PRIMARY KEY (type, id)
+);
+
+CREATE INDEX IF NOT EXISTS links_account_id_idx ON links (account_id);
+CREATE INDEX IF NOT EXISTS links_id_idx ON links (id);
+
+CREATE TABLE IF NOT EXISTS account_creations (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  link_type SMALLSERIAL NOT NULL,
+  link_id VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  UNIQUE (link_type, link_id)
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  id VARCHAR(255) PRIMARY KEY,
+  account_id UUID NOT NULL REFERENCES accounts(id),
+
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX IF NOT EXISTS sessions_account_id_idx ON sessions (account_id);
+
+CREATE TABLE IF NOT EXISTS addresses (
+  account_id UUID NOT NULL REFERENCES accounts(id),
+  address VARCHAR(255) NOT NULL,
+  encryption_method SMALLSERIAL NOT NULL,
+
+  PRIMARY KEY (account_id, address)
+);
+
+CREATE INDEX IF NOT EXISTS addresses_account_id_idx ON addresses (account_id);
+CREATE INDEX IF NOT EXISTS addresses_address_idx ON addresses (address);
+
+CREATE TABLE IF NOT EXISTS logs (
+  account_id UUID NOT NULL REFERENCES accounts(id),
+  action VARCHAR(255) NOT NULL,
+  data TEXT,
+  encryption_method SMALLSERIAL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
