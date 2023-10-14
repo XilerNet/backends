417dd6b1c823b4cf648343642bc81aef35d7a34b
arthurdw <dev@arthurdw.com>
2023-10-14 20:13:24 +0200

    :sparkles: Added refference to the payment inscription content

--- crates/db/migrations/20230915182552_create_private_keys.up.sql
+++ crates/db/migrations/20230915182552_create_private_keys.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS private_keys (
   account_id UUID NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+  payment_inscription_content_id UUID REFERENCES payment_inscription_contents(id),
   domain VARCHAR(255) NOT NULL,
   encryption_method SMALLSERIAL NOT NULL,
   private_key TEXT NOT NULL,
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
- stmux -- [ [ "cargo run --bin pay" .. "cargo run --bin auth" ] : "cargo run --bin domains"  ]
+ stmux -- [ [ "cargo run --bin pay" .. "cargo run --bin auth" ] : "DATABASE_URL="postgresql://postgres:alderson@localhost:5432/bitcoin" cargo run --bin domains" ]
